213f8d8e8d13366b36de50fe032cea7f2049e97c
Saurabh patel <saurabhpatel5061456@gmail.com>
2024-01-27 11:08:35 +0530

    update

--- src/app/api/coins/search/route.js
+++ src/app/api/coins/search/route.js
@@ -4,7 +4,7 @@ export async function fetchCoin(){
     const response = await fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0',{
         method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '0fc2dfad54mshf4ca7c17b1cf486p15dddajsnd597948d06f8',
+		'X-RapidAPI-Key':  process.env.RAPID_API_KEY,
 		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 	}
 
--- src/app/components/Coin.jsx
+++ src/app/components/Coin.jsx
@@ -1,65 +1,38 @@
 
-// import React from 'react'
-// import Image from 'next/image'
-// const Coin = ({coins}) => {
-//   return (
-//     // <div>
-//     //     {/* <div>
-//     //         {coins.map(item => (
-//     //             <li key={item.uuid}>
-//     //              <h1>{item.name}</h1>
-//     //              <p>{item.symbol}</p>
-//     //              <p>{item.price}</p>
-//     //             </li>
-//     //         ))}
-//     //     </div> */}
-//     // </div>
-
-//     <ul className="grid grid-cols-4 mx-auto max-w-[1260px] gap-10">
-//     {coins.map((coin => (
-//            <li key={coin.uuid} className="flex flex-col">
-//                <Image
-//        src={coin.iconUrl}
-//        alt={coin.name}
-//        width={70}
-//        height={70}
-//        priority
-//      />
-//                <h3>{coin.name}</h3>
-//                <p>{coin.symbol}</p>
-//                <p>{coin.price}</p>
-//            </li>
-//        )))}
-// </ul>
-//   )
-// }
-
-// export default Coin
-
-
-import Image from "next/image";
-
-
-export default function Coins({coins}) {
-
+import React from 'react'
+import Image from 'next/image'
+const Coin = ({coins}) => {
   return (
-    <>
-     <ul className="grid grid-cols-4 mx-auto max-w-[1260px] gap-10">
-         {coins.map(coin => (
-                <li key={coin.uuid} className="flex flex-col">
-                    <Image
-            src={coin.iconUrl}
-            alt={coin.name}
-            width={70}
-            height={70}
-            priority
-          />
-                    <h3>{coin.name}</h3>
-                    <p>{coin.symbol}</p>
-                    <p>{coin.price}</p>
-                </li>
-            ))}
-     </ul>
-    </>
+    // <div>
+    //     {/* <div>
+    //         {coins.map(item => (
+    //             <li key={item.uuid}>
+    //              <h1>{item.name}</h1>
+    //              <p>{item.symbol}</p>
+    //              <p>{item.price}</p>
+    //             </li>
+    //         ))}
+    //     </div> */}
+    // </div>
+
+    <ul className="grid grid-cols-4 mx-auto max-w-[1260px] gap-10">
+    {coins.map((coin => (
+           <li key={coin.uuid} className="flex flex-col">
+               <Image
+       src={coin.iconUrl}
+       alt={coin.name}
+       width={70}
+       height={70}
+       priority
+     />
+               <h3>{coin.name}</h3>
+               <p>{coin.symbol}</p>
+               <p>{coin.price}</p>
+           </li>
+       )))}
+</ul>
   )
 }
+
+export default Coin
+
